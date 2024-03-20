5ba5363b61a684fdd69050ea72970b9cd71d5558
natdm02 <natalidimarino2002>
2024-03-20 15:57:02 +0100

    [add] first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return{
+      title: 'Hello world!'
+    }
+    
+  },
+}
+</script>
+
+<template>
+
+  <div>
+
+        <h1>{{ title }}</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatem necessitatibus animi error similique expedita labore quas mollitia eaque veniam?</p>
+  </div>
+
+</template>
+
+<style>
+
+</style>
